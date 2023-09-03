26ff987bdc53e889912cff8d441f53ae4406d213
alexanderley <112874444+alexanderley@users.noreply.github.com>
2023-09-03 17:01:35 +0200

    changed the backend url for deploy

--- apiKey.tsx
+++ apiKey.tsx
@@ -1,7 +1,7 @@
 // local API
-const API_URL = "http://localhost:5005";
+// const API_URL = "http://localhost:5005";
 
 // cloud API !!!
-// const API_URL = "https://scoretwce-backend.onrender.com";
+const API_URL = "https://ts-pdf-uploader-backend.onrender.com";
 
 export default API_URL;
