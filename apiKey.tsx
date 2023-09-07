b8173b4e9014dceca0f91286dc3237599f2114fc
alexanderley <112874444+alexanderley@users.noreply.github.com>
2023-09-07 13:20:53 +0200

    netlify version backend

--- apiKey.tsx
+++ apiKey.tsx
@@ -2,6 +2,10 @@
 // const API_URL = "http://localhost:5005";
 
 // cloud API !!!
+// old backend
 const API_URL = "https://ts-pdf-uploader-backend.onrender.com";
+
+// new backend
+const API_URL = "https://netlify--exquisite-babka-de18a1.netlify.app/";
 
 export default API_URL;
