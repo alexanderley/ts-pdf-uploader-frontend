878383ff77de4ebcc49c1f44c40024e6193ff639
alexanderley <112874444+alexanderley@users.noreply.github.com>
2023-09-07 13:47:11 +0200

    latest commit

--- apiKey.tsx
+++ apiKey.tsx
@@ -3,7 +3,7 @@
 
 // cloud API !!!
 // old backend
-const API_URL = "https://ts-pdf-uploader-backend.onrender.com";
+// const API_URL = "https://ts-pdf-uploader-backend.onrender.com";
 
 // new backend
 const API_URL = "https://netlify--exquisite-babka-de18a1.netlify.app/";
